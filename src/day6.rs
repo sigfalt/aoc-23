198d1bc7dc319d5837e81b579e3b14ab8e3b6f52
sigfalt <1689082+sigfalt@users.noreply.github.com>
2023-12-06 20:29:26 +0000

    Add AOC day 6 part 2 solution

--- src/day6.rs
+++ src/day6.rs
@@ -1,6 +1,7 @@
+use anyhow::Result;
 use aoc_runner_derive::aoc;
 use itertools::Itertools;
-use nom::{IResult, combinator::{map_res, all_consuming}, multi::separated_list1, character::complete::{digit1, space1, alpha1, char, line_ending}, sequence::{preceded, tuple, separated_pair}};
+use nom::{IResult, combinator::{map_res, all_consuming}, multi::{separated_list1, fold_many1}, character::complete::{digit1, space1, alpha1, char, line_ending}, sequence::{preceded, tuple, separated_pair, pair}};
 
 
 fn parse_u32(input: &str) -> IResult<&str, u32> {
@@ -24,16 +25,27 @@ fn parse_input_line(input: &str) -> IResult<&str, Vec<u32>> {
 
 #[derive(Debug, PartialEq)]
 struct Race {
-    time: u32,
-    distance: u32
+    time: u64,
+    distance: u64
 }
 impl From<(u32, u32)> for Race {
     fn from((time, distance): (u32, u32)) -> Self {
+        Self{time: time.into(), distance: distance.into()}
+    }
+}
+impl TryFrom<(i32, i32)> for Race {
+    type Error = anyhow::Error;
+    fn try_from((time, distance): (i32, i32)) -> Result<Race> {
+        Ok(Self{time: time.try_into()?, distance: distance.try_into()?})
+    }
+}
+impl From<(u64, u64)> for Race {
+    fn from((time, distance): (u64, u64)) -> Self {
         Self{time, distance}
     }
 }
 
-fn parse(input: &str) -> impl Iterator<Item = Race> {
+fn parse_part1(input: &str) -> impl Iterator<Item = Race> {
     let (_, (times, distances)) = all_consuming(separated_pair(
         parse_input_line,
         line_ending,
@@ -43,9 +55,36 @@ fn parse(input: &str) -> impl Iterator<Item = Race> {
     times.into_iter().zip_eq(distances.into_iter()).map(|r| r.into())
 }
 
+fn parse_separated_u64(input: &str) -> IResult<&str, u64> {
+    map_res(fold_many1(
+        preceded(space1, digit1),
+        String::new,
+        |acc, digits| {
+            acc + digits
+        }
+    ), |num| u64::from_str_radix(&num, 10))(input)
+}
+
+fn parse_bad_kerning_line(input: &str) -> IResult<&str, u64> {
+    preceded(
+        pair(alpha1, char(':')),
+        parse_separated_u64
+    )(input)
+}
+
+fn parse_part2(input: &str) -> Race {
+    let (_, race) = all_consuming(separated_pair(
+        parse_bad_kerning_line,
+        line_ending,
+        parse_bad_kerning_line
+    ))(input).unwrap();
+
+    race.into()
+}
+
 #[aoc(day6, part1)]
-fn part1(input: &str) -> u32 {
-    let races = parse(input);
+fn part1(input: &str) -> u64 {
+    let races = parse_part1(input);
 
     races.map(|race| {
         // check from 1 to time/2 as that will be the maximum distance
@@ -58,10 +97,13 @@ fn part1(input: &str) -> u32 {
 }
 
 #[aoc(day6, part2)]
-fn part2(input: &str) -> u32 {
-    let races = parse(input);
+fn part2(input: &str) -> u64 {
+    let race = parse_part2(input);
 
-    todo!()
+    let min_time = (1..race.time / 2).find(|button_time|
+        button_time * (race.time - button_time) > race.distance
+    ).unwrap();
+    ((race.time / 2 + 1) - min_time) * 2 - (if race.time % 2 == 0 { 1 } else { 0 })
 }
 
 
@@ -71,8 +113,12 @@ mod tests {
 
     #[test]
     fn parser_test() {
-        assert_eq!(parse("Time:      7  15   30
-Distance:  9  40  200").collect_vec(), vec![Race::from((7, 9)), Race::from((15, 40)), Race::from((30, 200))]);
+        assert_eq!(parse_part1("Time:      7  15   30
+Distance:  9  40  200").collect_vec(), vec![Race::try_from((7, 9)).unwrap(), Race::try_from((15, 40)).unwrap(), Race::try_from((30, 200)).unwrap()]);
+
+        assert_eq!(parse_separated_u64("      7  15   30"), Ok(("", 71530)));
+        assert_eq!(parse_part2("Time:      7  15   30
+Distance:  9  40  200"), Race::try_from((71530, 940200)).unwrap());
     }
 
     #[test]
